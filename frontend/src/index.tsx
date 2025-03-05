3b62d018346a056814abd410443ae6cc42c7a52f
Vladimir Skudin <skudinva@yandex.ru>
2025-03-05 21:39:27 +0700

    skelet

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+import { store } from './store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// Функция для рендеринга приложения
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement as Element);
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <Provider store={store}>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </Provider>
   </React.StrictMode>
 );
