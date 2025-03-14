e39ffda94da0a657e4dc62c4f2f844da0c9c3779
Vladimir Skudin <skudinva@yandex.ru>
2025-03-14 12:15:05 +0700

    ++

---
+++ frontend/src/components/login-form/login-form.tsx
@@ -0,0 +1,56 @@
+import { FormEvent, useEffect } from 'react';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import history from '../../history';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { loginUser } from '../../store/action';
+import { getAuthorizationStatus } from '../../store/user-process/selectors';
+import { LoginUserDto } from '../../types/shared';
+
+function LoginForm(): JSX.Element {
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (authorizationStatus === AuthorizationStatus.Auth) {
+      history.push(AppRoute.Root);
+    }
+  }, [authorizationStatus]);
+
+  const onFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data: LoginUserDto = {
+      email: formData.get('email')?.toString() || '',
+      password: formData.get('password')?.toString() || '',
+    };
+    dispatch(loginUser(data));
+  };
+
+  return (
+    <form method="get" onSubmit={onFormSubmit}>
+      <div className="sign-in">
+        <div className="custom-input sign-in__input">
+          <label>
+            <span className="custom-input__label">E-mail</span>
+            <span className="custom-input__wrapper">
+              <input type="email" name="email" />
+            </span>
+          </label>
+        </div>
+        <div className="custom-input sign-in__input">
+          <label>
+            <span className="custom-input__label">Пароль</span>
+            <span className="custom-input__wrapper">
+              <input type="password" name="password" />
+            </span>
+          </label>
+        </div>
+        <button className="btn sign-in__button" type="submit">
+          Продолжить
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,31 +1,19 @@
-import { FormEvent, useEffect } from 'react';
+import { useEffect } from 'react';
 import CustomHelmet from '../../components/custom-helmet/custom-helmet';
+import LoginForm from '../../components/login-form/login-form';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import history from '../../history';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import { loginUser } from '../../store/action';
+import { useAppSelector } from '../../hooks';
 import { getAuthorizationStatus } from '../../store/user-process/selectors';
-import { LoginUserDto } from '../../types/shared';
 
 function Login(): JSX.Element {
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
-  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (authorizationStatus === AuthorizationStatus.Auth) {
       history.push(AppRoute.Root);
     }
   }, [authorizationStatus]);
-
-  const onFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const data: LoginUserDto = {
-      email: formData.get('email')?.toString() || '',
-      password: formData.get('password')?.toString() || '',
-    };
-    dispatch(loginUser(data));
-  };
 
   return (
     <>
@@ -55,29 +43,7 @@ function Login(): JSX.Element {
               <h1 className="popup-form__title">Вход</h1>
             </div>
             <div className="popup-form__form">
-              <form method="get" onSubmit={onFormSubmit}>
-                <div className="sign-in">
-                  <div className="custom-input sign-in__input">
-                    <label>
-                      <span className="custom-input__label">E-mail</span>
-                      <span className="custom-input__wrapper">
-                        <input type="email" name="email" />
-                      </span>
-                    </label>
-                  </div>
-                  <div className="custom-input sign-in__input">
-                    <label>
-                      <span className="custom-input__label">Пароль</span>
-                      <span className="custom-input__wrapper">
-                        <input type="password" name="password" />
-                      </span>
-                    </label>
-                  </div>
-                  <button className="btn sign-in__button" type="submit">
-                    Продолжить
-                  </button>
-                </div>
-              </form>
+              <LoginForm />
             </div>
           </div>
         </div>
