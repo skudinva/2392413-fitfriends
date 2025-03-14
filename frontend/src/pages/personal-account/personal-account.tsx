f00fc5aaa5364ed1ce7684543e7610abdb3e6199
Vladimir Skudin <skudinva@yandex.ru>
2025-03-14 12:05:40 +0700

    ++

--- frontend/src/pages/personal-account/personal-account.tsx
+++ frontend/src/pages/personal-account/personal-account.tsx
@@ -2,20 +2,16 @@ import CustomHelmet from '../../components/custom-helmet/custom-helmet';
 import PersonalAccountUser from '../../components/personal-account-user/personal-account-user';
 import Spinner from '../../components/spinner/spinner';
 import UserInfoForm from '../../components/user-info-form/user-info-form';
+import UserInfoHeader from '../../components/user-info-header/user-info-header';
 import { useAppSelector } from '../../hooks';
-import {
-  getUserInfo,
-  getUserInfoLoading,
-} from '../../store/user-process/selectors';
+import { getUserInfoLoading } from '../../store/user-process/selectors';
 
 function PersonalAccount(): JSX.Element {
-  const userInfo = useAppSelector(getUserInfo);
   const userInfoLoading = useAppSelector(getUserInfoLoading);
 
   if (userInfoLoading) {
     return <Spinner />;
   }
-
   return (
     <section className="inner-page">
       <CustomHelmet pageTitle="Личный кабинет" />
@@ -23,45 +19,7 @@ function PersonalAccount(): JSX.Element {
         <div className="inner-page__wrapper">
           <h1 className="visually-hidden">Личный кабинет</h1>
           <section className="user-info">
-            <div className="user-info__header">
-              <div className="input-load-avatar">
-                <label>
-                  <input
-                    className="visually-hidden"
-                    type="file"
-                    name="user-photo-1"
-                    accept="image/png, image/jpeg"
-                  />
-                  <span className="input-load-avatar__avatar">
-                    <img
-                      src={userInfo?.avatar}
-                      srcSet={userInfo?.avatar && `${userInfo.avatar} 2x`}
-                      width="98"
-                      height="98"
-                      alt="user photo"
-                    />
-                  </span>
-                </label>
-              </div>
-              <div className="user-info-edit__controls">
-                <button
-                  className="user-info-edit__control-btn"
-                  aria-label="обновить"
-                >
-                  <svg width="16" height="16" aria-hidden="true">
-                    <use xlinkHref="#icon-change"></use>
-                  </svg>
-                </button>
-                <button
-                  className="user-info-edit__control-btn"
-                  aria-label="удалить"
-                >
-                  <svg width="14" height="16" aria-hidden="true">
-                    <use xlinkHref="#icon-trash"></use>
-                  </svg>
-                </button>
-              </div>
-            </div>
+            <UserInfoHeader />
             <UserInfoForm />
           </section>
           <div className="inner-page__content">
