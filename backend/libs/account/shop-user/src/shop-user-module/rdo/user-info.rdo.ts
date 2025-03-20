a11aeb8b593738d96679c399bf6298aa760beedf
Vladimir Skudin <skudinva@yandex.ru>
2025-03-20 10:59:51 +0700

    ++

--- backend/libs/account/shop-user/src/shop-user-module/rdo/user-info.rdo.ts
+++ backend/libs/account/shop-user/src/shop-user-module/rdo/user-info.rdo.ts
@@ -1,25 +1,14 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
+import { UserRdo } from './user.rdo';
 
-export class UserInfoRdo {
-  @ApiProperty({
-    description: 'The uniq user ID',
-    example: '134ce8babd-cc30-4805-9b12-d9420398e7c5',
-  })
+export class UserInfoRdo extends PickType(UserRdo, ['id', 'email', 'name']) {
   @Expose()
   public id!: string;
 
-  @ApiProperty({
-    description: 'User email',
-    example: 'user@user.local',
-  })
   @Expose()
   public email!: string;
 
-  @ApiProperty({
-    description: 'User name',
-    example: 'Keks',
-  })
   @Expose()
   public name!: string;
 }
