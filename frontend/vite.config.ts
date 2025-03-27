811dd6bf9070455c68ab46647ff140c191a739bf
Vladimir Skudin <skudinva@yandex.ru>
2025-03-27 21:36:49 +0700

    ++

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,23 @@
 /// <reference types='vitest' />
 /// <reference types='vite/client' />
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: 'http://localhost:5173',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
